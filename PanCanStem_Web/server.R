a6ff0b59373c8bcf6c12da0d93ceaa87d663dd56
tiago <tiagochst@gmail.com>
2017-02-10 18:33:18 -0200

    Starting code

--- PanCanStem_Web/server.R
+++ PanCanStem_Web/server.R
@@ -10,43 +10,62 @@
 library(shiny)
 
 library(fgsea)
-load("data/pd.all.Rda")
-load("data/pd.merge.Rda")
 load("data/pd.450.prim_20170207.Rda")
 load("data/pd.maf.450.Rda")
 load("data/pd.maf.RNA.Rda")
 load("data/pd.mRNA.prim_20170208.Rda")
 set.seed(10)
-cancer.type <- "ACC"
-gene <- "A2ML1"
-test <- subset(pd.maf.450, cancer.type %in% levels(pd.all$cancer.type) & sample.type %in% "01") #653  19705 or #672
-test <- droplevels(test)
-stats <- test[order(test$DNAss),] #rank samples
-stats <- structure(stats$DNAss, names=rownames(stats))
-pathways <- as.list(unstack(test[,c("TCGAlong.id", gene )]))
-#
-stats <- test[order(test$DNAss),] #rank samples
-stats <- structure(stats$DNAss, names=rownames(stats))
-#
-result <- fgsea(pathways = pathways, stats = stats,  nperm=100, minSize=5, maxSize=500)
-#
-plotEnrichment(pathways[["Missense_Mutation"]], stats) + labs(title="IDH1-Mutant")
-plotEnrichment(pathways[["WT"]], stats) + labs(title="IDH1-WT")
-#
-plotGseaTable(pathways, stats, result,  gseaParam = 0.5)
+
+
 
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+shinyServer(function(input, output,session) {
    
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
+  
+  observeEvent(input$experiment, {
+    # This must be checked how do we have smoking?
+    updateSelectizeInput(session, 'feature', choices = {
+      if(input$experiment == "Gene expression") ret <- as.character(sort(unique(pd.maf.450$cancer.type)))
+      if(input$experiment == "DNA methylation") ret <- as.character(sort(unique(pd.maf.RNA$cancer.type)))
+      ret
+    }, server = TRUE)
+  })
+  
+  volcano.values <- reactive({
+    feature <- "IDH1"
+    if(isolate({input$experiment}) == "Gene expression")  {
+      pd <- pd.maf.RNA
+      primary <- pd.mRNA.prim[pd.mRNA.prim$sample.type %in% c("01","03"),"TCGAlong.id"]
+      col <- "RNAss"
+    }
+    if(isolate({input$experiment})  == "DNA methylation")  {
+      pd <- pd.maf.450
+      primary <- pd.450.prim[pd.450.prim$sample.type %in% c("01","03"),"TCGAlong.id"]
+      col <- "DNAss"
+    }
+    test <- subset(pd, cancer.type %in% levels(pd$cancer.type) & TCGAlong.id %in% primary) 
+    test <- droplevels(test)
+    stats <- test[order(test[,col]),] #rank samples
+    stats <- structure(stats[,col], names=rownames(stats))
+    pathways <- as.list(unstack(test[,c("TCGAlong.id", feature )]))
+    withProgress(message = 'Creating plot',
+                 detail = 'This may take a while...', value = 0, {
+    result <- fgsea(pathways = pathways, stats = stats,  nperm=10, minSize=5, maxSize=500)
+    })
+    ret <- list(stats = stats, 
+                pathways = pathways)
+    return(result)
+  })
+  
+  observeEvent(input$plot , {
+    print("Hey2")
+    output$distPlot <- renderPlot({
+     print("Hey")
+     ret <- volcano.values()
+     plotEnrichment(ret$pathways[["Missense_Mutation"]], ret$stats) + labs(title="IDH1-Mutant")
+     #plotEnrichment(pathways[["WT"]], stats) + labs(title="IDH1-WT")
+     #plotGseaTable(pathways, stats, result,  gseaParam = 0.5)
+   })
   })
   
 })
--- PanCanStem_Web/ui.R
+++ PanCanStem_Web/ui.R
@@ -9,6 +9,16 @@
 
 library(shiny)
 
+getTCGAdisease <- function(){
+  projects <- TCGAbiolinks:::getGDCprojects()
+  disease <-  projects$project_id
+  names(disease) <-  paste0(projects$disease_type, " (",disease,")")
+  disease <- disease[sort(names(disease))]
+  tcga.disease <- disease[grep("TCGA",disease)]
+  tcga.disease <- gsub("TCGA-","",tcga.disease)
+  tcga.disease <- tcga.disease[sort(names(tcga.disease), index.return=TRUE)$ix]
+  return(tcga.disease)
+}
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
@@ -18,16 +28,30 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+      selectizeInput('experiment',
+                     'Experiment filter',
+                     c("Gene expression","DNA methylation"),
+                     multiple = FALSE),
+      selectizeInput('cancertype',
+                     'Cancer type',
+                     getTCGAdisease(),
+                     multiple = FALSE),
+      selectizeInput('feature',
+                     'Feature',
+                     NULL,
+                     multiple = FALSE),
+      actionButton("plot",
+                   "Plot",
+                   style = "background-color: #000080;
+                                    color: #FFFFFF;
+                                    margin-left: auto;
+                                    margin-right: auto;
+                                    width: 100%",
+                   icon = icon("flask"))
     ),
-    
     # Show a plot of the generated distribution
     mainPanel(
-       plotOutput("distPlot")
+      plotOutput("distPlot")
     )
   )
 ))
